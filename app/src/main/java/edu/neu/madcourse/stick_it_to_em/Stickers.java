6090cbc91e48b1dde20cc0618d5b58d1542defb9
Varshitha Uppula <varshithauppula@gmail.com>
2022-07-09 20:44:44 -0400

    sticker activity created

---
+++ app/src/main/java/edu/neu/madcourse/stick_it_to_em/Stickers.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.stick_it_to_em;
+
+import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class Stickers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stickers);
+    }
+}
