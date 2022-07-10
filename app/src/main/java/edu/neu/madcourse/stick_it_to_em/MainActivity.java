9decc3ef6fd95214ff2f3735b5e866d31b8b1c5d
Srihitha <saisrihithay@gmail.com>
2022-07-10 15:15:09 -0400

    login and registration functionality

--- app/src/main/java/edu/neu/madcourse/stick_it_to_em/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/stick_it_to_em/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.neu.madcourse.stick_it_to_em;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.register:
+                Intent intent0 = new Intent(this, RegistrationActivity.class);
+                startActivity(intent0);
+                break;
+
+            case R.id.login:
+                Intent intent1 = new Intent(this, LoginActivity.class);
+                startActivity(intent1);
+                break;
+        }
     }
 }
